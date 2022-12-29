18402c7006cae24882c3ebe86242e8ffe1018241
youness-elbaz <116465114+youness-elbaz@users.noreply.github.com>
2022-12-29 21:42:42 +0100

    compenenant

---
+++ src/compenant/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        Designed and developed by Belhaj-Infos
+        <span>🧡</span>
+    </footer>
+  )
+}
+
+export default Footer
